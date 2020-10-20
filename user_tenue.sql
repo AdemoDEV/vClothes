bb58bebac68a3d4ae117454c4a7f415f7af02207
AdeMo <58342559+AdemoDEV@users.noreply.github.com>
2020-10-20 16:44:49 +0200

    Add files via upload

---
+++ user_tenue.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 20 oct. 2020 à 16:44
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `vm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_tenue`
+--
+
+CREATE TABLE `user_tenue` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `tenue` longtext COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
